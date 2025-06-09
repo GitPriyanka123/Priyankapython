ddab5707a51287c2538277e23d042a89d82fbecc
GitPriyanka123 <priyanka.reply13@gmail.com>
2025-06-09 20:47:57 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+x=int(input("Enter the first number"))
+y=int(input("Enter the second number"))
+
+a=x+y
+s=x-y
+m=x*y
+d=x/y
+
+print("Addition",a)
+print("Subtraction",s)
+print("Multiplication",m)
+print("Division",d)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+f=input("Enter First name")
+l=input("Enter last name")
+
+print(f"Hello, {f+l}! Welcome to the python program") 
